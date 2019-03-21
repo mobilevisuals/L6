9fa28f79a96974addc03ef3865612d0c80dfe9e8
mobilevisuals <eyvind4@gmail.com>
2019-03-21 18:45:57 +0100

    sdfsdf

---
+++ src/java/UserController.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Admin
+ */
+@Named(value = "userController")
+@SessionScoped
+public class UserController implements Serializable {
+    
+    private String name;
+    private String password;
+
+
+    /**
+     * Creates a new instance of UserController
+     */
+    public UserController() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
+}
